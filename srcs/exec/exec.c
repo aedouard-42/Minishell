a64662e2d675aeec52ecfc268c2c2b4d13b4252a
Antoine EDOUARD <aedouard@e2r4p17.clusters.42paris.fr>
2021-12-20 20:18:47 +0100

    clean exec

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -1,60 +1,36 @@
 #include "../../includes/minishell.h"
 
-int pipe_cmd(t_cmd *cmd, t_cmd *prev_cmd, t_cmd *next_cmd)
-{
-    int type;
-
-    type = cmd->type;
-	//printf("type : %d\n", type);
-
-	pipe(cmd->fd);
-	if (prev_cmd == NULL)
-	{
-		printf("first pipe\n");
-		//pipe_first(cmd);
-	}
-	if (next_cmd && prev_cmd != NULL)
-		perror("mid pipe\n");
-	if (next_cmd == NULL && prev_cmd != NULL)
-		perror("last pipe");
-
-	return (1);
-}
-
 int execute_cmd(t_cmd *cmd,t_cmd *prev, t_cmd *next, char **envp)
 {
 	pid_t pid;
 	int status;
 
-	pid = fork();
-	if (pid == 0)
-    {
-        if (next != NULL)
-            dup2(cmd->fd[1], STDOUT_FILENO);
-        if (prev != NULL)
-            dup2(prev->fd[0], STDIN_FILENO);
-    	execve(cmd->args[0], cmd->args, envp);
-        printf("execve failed for test : %s\n", cmd->args[0]);
-        exit(1);
-	}
-	else
+    if (next != NULL)
+        dup2(cmd->fd[1], STDOUT_FILENO);
+    if (prev != NULL)
+        dup2(prev->fd[0], STDIN_FILENO);
+    execve(cmd->args[0], cmd->args, envp);
+    printf("execve failed for test : %s\n", cmd->args[0]);
+    exit(1);
+    return (1);
+}
+
+int exec_cmd(t_cmd *cmd, t_cmd *prev, t_cmd *next_cmd, char **envp)
+{
+    pid_t pid;
+    int status;
+
+    pipe(cmd->fd);
+    pid = fork();
+    if (pid == 0)
+        execute_cmd(cmd, prev, next_cmd, envp);
+    else
     {
         waitpid(pid, &status, 0);
         close(cmd->fd[1]);
         if (prev && prev->type  == PIPE)
 			close(prev->fd[0]);
     }
-    return (1);
-}
-
-int exec_cmd(t_cmd *cmd, t_cmd *prev_cmd, t_cmd *next_cmd, char **envp)
-{
-    pid_t pid;
-    int status;
-
-    //pipe_cmd(cmd, prev_cmd, next_cmd);
-    pipe(cmd->fd);
-    execute_cmd(cmd, prev_cmd, next_cmd, envp);
     return (1);
 }
 
